1c9f4ce372687b9be2599590eca7362400e8e6ee
quyennvj <162701182+quyennvj@users.noreply.github.com>
2024-05-07 20:46:02 +0700

    nhac

---
+++ music-list.js
@@ -0,0 +1,97 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Có Một Nơi Như Thế",
+    artist: "Phan Mạnh Quỳnh",
+    img: "Có Một Nơi Như Thế",
+    src: "Có Một Nơi Như Thế"
+  },
+  {
+    name: "BẠN ĐỜI",
+    artist: "KARIK",
+    img: "BẠN ĐỜI",
+    src: "BẠN ĐỜI"
+  },
+  
+  {
+    name: "Vì chúng ta là con người",
+    artist: "Ronboogz",
+    img: "Vì chúng ta là con người",
+    src: "Vì chúng ta là con người"
+  },
+  {
+    name: "24H",
+    artist: "LYLY",
+    img: "24H",
+    src: "24H"
+  },
+  {
+    name: "sài gòn buồn quá em ơi",
+    artist: "Dế choắt x Jason Ehh",
+    img: "sài gòn buồn quá em ơi",
+    src: "sài gòn buồn quá em ơi"
+  },
+  {
+    name: "Crying Over You",
+    artist: "JustaTee ft. Binz",
+    img: "Crying Over You",
+    src: "Crying Over You"
+  },
+  {
+    name: "Solo",
+    artist: "myles smith",
+    img: "Solo",
+    src: "Solo"
+
+  },
+  {
+    name: "Cơn Mưa Cuối",
+    artist: "JustaTee x Binz",
+    img: "Cơn Mưa Cuối",
+    src: "Cơn Mưa Cuối"
+
+  },
+  
+  {
+    name: "Forget Me Now",
+    artist: "Fishy x Trí Dũng",
+    img: "Forget Me Now",
+    src: "Forget Me Now"
+
+  },
+  {
+    name: "Đừng Đổ Lỗi Bọn Trẻ",
+    artist: "B Ray x Hoàng Yến Chibi",
+    img: "Đừng Đổ Lỗi Bọn Trẻ",
+    src: "Đừng Đổ Lỗi Bọn Trẻ"
+
+  },
+  {
+    name: "SAIGON VÀ EM 5",
+    artist: "Minh Huy, CoZi",
+    img: "SAIGON VÀ EM 5",
+    src: "SAIGON VÀ EM 5"
+
+  },
+  {
+    name: "Anh Đã Lớn Hơn Thế Nhiều",
+    artist: "feat. Michelle Ngn",
+    img: "Anh Đã Lớn Hơn Thế Nhiều",
+    src: "Anh Đã Lớn Hơn Thế Nhiều"
+
+  },
+  {
+  name: "I Took A Pill In Ibiza",
+  artist: "Mike Posner",
+  img: "I Took A Pill In Ibiza",
+  src: "I Took A Pill In Ibiza"
+  },
+]
